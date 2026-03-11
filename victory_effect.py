c2f6f77c01b9de942fa1caaabddd927a957aa804
Hervé Noukpo Houndjetodé <vetcho006@hotmail.com>
2026-03-11 11:34:34 +0100

    refactor: improved animation

--- victory_effect.py
+++ victory_effect.py
@@ -1,4 +1,11 @@
 import time
+import os
+
+
+def clear_console():
+    # mac/linux = clear, windows = cls
+    os.system("cls" if os.name == "nt" else "clear")
+
 
 def effet_victoire():
     frame_up = [
@@ -15,11 +22,12 @@ def effet_victoire():
 
     print("YOUPIIII !!!")
     for _ in range(6):  # nombre de mouvements
-      
+        clear_console()
         print("YOUPIIII !!!")
         print("\n".join(frame_up))
         time.sleep(0.25)
 
+        clear_console()
         print("YOUPIIII !!!")
         print("\n".join(frame_down))
         time.sleep(0.25)
